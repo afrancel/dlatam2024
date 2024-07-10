3766456f754f7a1bf82f3e4164e3aeac819f3867
afrancel <hi@afrancel.com>
2024-07-10 03:06:27 -0400

    update desafio

--- III_js_04/1_funcion.js
+++ III_js_04/1_funcion.js
@@ -1,5 +1,17 @@
-function example(a,b,c){
-  return a+b+c
+/*Transforma esta declaración de función
+a una función de expresión. El ejercicio
+debe quedar en un archivo nombrado `1_funcion.js` (1 Punto)
+*/
+
+function example(a, b, c) {
+  return a + b + c;
 }
 
-console.log(example(2,3,4));
+// Mi respuesta
+console.log(example(2, 3, 4));
+
+//Respuesta esperada
+const example = (a, b, c) => {
+  return a + b + c;
+};
+console.log(example(2, 3, 4));
--- III_js_04/5_keyDown.js
+++ III_js_04/5_keyDown.js
@@ -7,21 +7,38 @@ document.addEventListener("keydown", function (event) {
     keyColor = "Rosado";
     document.getElementById("key").style.backgroundColor = "pink";
     document.getElementById("key").innerHTML = mok;
-    
-
   } else if (event.key === "s") {
     keyColor = "orange";
     document.getElementById("key").style.backgroundColor = "orange";
     document.getElementById("key").innerHTML = mok;
-
   } else if (event.key === "d") {
     keyColor = "blue";
     document.getElementById("key").style.backgroundColor = "lightblue";
-        document.getElementById("key").innerHTML = mok;
-
+    document.getElementById("key").innerHTML = mok;
+  } else if (event.key === "q") {
+    createNewDiv("purple");
+  } else if (event.key === "w") {
+    createNewDiv("gray");
+  } else if (event.key === "e") {
+    createNewDiv("brown");
   } else {
-    document.getElementById("key").style.backgroundColor = "white";    
+    document.getElementById("key").style.backgroundColor = "white";
     document.getElementById("key").innerHTML = merror;
-      
   }
 });
+
+/*Siguiendo con la lógica del punto anterior, al presionar
+las teclas q, w o e se deberá crear un div nuevo de las mismas
+dimensiones antes mencionadas con los colores morado, gris y café respectivamente"*/
+
+document.addEventListener("keydown", function (event) {
+
+});
+function createNewDiv(color) {
+  const newDiv = document.createElement("div");
+  newDiv.style.width = "200px";
+  newDiv.style.height = "200px";
+  newDiv.style.margin = "50px";
+  newDiv.style.backgroundColor = color;
+  document.body.appendChild(newDiv); /*ESTO NO LO VIMOS*/
+}
